e662378479cac4ccaa09c97f040aaa3e6be234cf
Tztedtrzh <120391182+BenChanlLOL@users.noreply.github.com>
2024-05-16 11:42:07 +0200

    Update loader.py bug fixes

--- loader.py
+++ loader.py
@@ -1,12 +1,12 @@
 #loader.py
 import socket
 sock = socket.socket()
-print(" the payload must be in the folder and called 'payload'"
+print(" the payload must be in the folder and called 'payload'")
 payload1 = open("payload")
-payload2 = payload1.read
+payload2 = payload1.read()
 con = input("what IP should we connect to?")
 port = int(input("what port"))
 sock.connect((con, port))
-sock.send(payload.encode(utf-8))
+sock.send(payload2.encode(utf-8))
 print("sent payload")
 sock.close()
